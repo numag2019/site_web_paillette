1370a8a01357244fc412cb2755e14c30796e76d9
MaxDmnt <maxime.dumont96@gmail.com>
2019-05-02 11:38:09 +0200

    Requete et création csv table eleveur
    Récupération des éleveurs dont l'élevage possède une des races de bovins étudiée dans un tableau php.
    Création d'un csv a partir de ce tableau php.

--- importexportCSV/creationcsv.php
+++ importexportCSV/creationcsv.php
@@ -11,7 +11,21 @@ $requete="SELECT id_animal,nom_animal,sexe FROM animal";
 		// JOIN Departements ON Communes.id_dpt=Departements.id_dpt and Departements.nom_dpt='".$dep."'
 		// WHERE MONTH(Observations.date) BETWEEN '".$date1."' and '".$date2."' and Observations.nombre >".$nbremin."
 		// GROUP BY Observations.date";
-		
+
+//
+// Requete Eleveurs  : on sélectionne seulement les éleveurs dont l'elevage possède la/les race(s) Bearnaise, Bordelaise, Marine.
+//(Bearnaise : 19, Bordelaise : 6, Marine : 6)
+//
+$requete="
+SELECT id_contact,nom,prenom FROM contact
+JOIN link_race_elevage ON contact.id_elevage = link_race_elevage.id_elevage
+
+WHERE link_race_elevage.code_race=19 OR link_race_elevage.code_race=6  OR link_race_elevage.code_race=5
+
+";
+
+
+// Tableau Eleveurs		
 $obs=mysqli_query($link,$requete);
 
 // Transformation données en tableau 
@@ -23,7 +37,7 @@ $nbcol=mysqli_num_fields($obs);
 
 // Les lignes du tableau
 $i=0;
-	while ($i<5)
+	while ($i<nbligne)
 	{	
 		$lignes[] = array('/'.$tab[$i][0], $tab[$i][1], $tab[$i][2].'/');
 		$i++;
@@ -35,22 +49,22 @@ $chemin = 'csv/fichier.csv';
 
 // Création du fichier csv (le fichier est vide pour le moment)
 // w+ : consulter http://php.net/manual/fr/function.fopen.php
-$fichier_csv = fopen($chemin, 'w+');
+$table_eleveurs_csv = fopen($chemin, 'w+');
 
 // Si votre fichier a vocation a être importé dans Excel,
 // vous devez impérativement utiliser la ligne ci-dessous pour corriger
 // les problèmes d'affichage des caractères internationaux (les accents par exemple)
-fprintf($fichier_csv, chr(0xEF).chr(0xBB).chr(0xBF));
+fprintf($table_eleveurs_csv, chr(0xEF).chr(0xBB).chr(0xBF));
 
 // Boucle foreach sur chaque ligne du tableau
 foreach($lignes as $ligne){
 	// chaque ligne en cours de lecture est insérée dans le fichier
 	// les valeurs présentes dans chaque ligne seront séparées par $delimiteur
-	fputcsv($fichier_csv,$ligne,";","\"");
+	fputcsv($table_eleveurs_csv,$ligne,";","\"");
 }
 
 // fermeture du fichier csv
-fclose($fichier_csv);
+fclose($table_eleveurs_csv);
 ?>
 
 

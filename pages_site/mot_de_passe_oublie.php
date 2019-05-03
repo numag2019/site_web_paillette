260b12a97a03b35f410c16a2c99e012adf58200d
theo6197@gmail.com <49755574+theo6197@users.noreply.github.com>
2019-05-03 09:09:12 +0200

    modif

--- pages_site/mot_de_passe_oublie.php
+++ pages_site/mot_de_passe_oublie.php
@@ -1,5 +1,16 @@
+<html>
+	<head>
+		<!-- informations sur le type et la version du fichier HTML et informations diverses à destination notamment 				des robots-->
+		<meta charset="UTF-8">
+	</head>
+	
+	<body>
+	<?php
+	include(../mise_en_page/entete.php)
+	?>
 <p>
-    Réinitialisation du mot de passe <br />
+    Réinitialisation du mot de passe 
+	<br />
     Veuillez entrer votre adresse email :
 </p>
 
@@ -16,14 +27,15 @@ if(isset($_POST['email']))
 {
 $mail = $_POST['email']; // Déclaration de l'adresse de destination.
 
-if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mail)) // On filtre les serveurs qui rencontrent des bogues.
-{
+	if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mail)) // On filtre les serveurs qui rencontrent des bogues.
+	{
 	$passage_ligne = "\r\n";
-}
+	}
 else
-{
+	{
 	$passage_ligne = "\n";
-}
+	}
+
 //=====Déclaration des messages au format texte et au format HTML.
 $message_txt = 'Veuillez trouvez ci dessous le mot de passe temporaire de votre comptre cranet, pensez a le changer rapidement';
 $mdp='motdepasseturfu45';
@@ -46,3 +58,8 @@ echo "Votre nouveau mot de passe a bien été envoyé à l'adresse ".$mail;
 //==========
 }
 ?>
+	</body>
+	<?php
+	include (../mise_en_page/pied.php)
+	?>
+</html>

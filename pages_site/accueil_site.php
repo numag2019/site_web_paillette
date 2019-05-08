557c747a1c0ad88706887a4e884e2e77cb4d70c9
pierre-alban <50011360+pierre-alban@users.noreply.github.com>
2019-05-08 17:49:20 +0200

    ajout style bootstrap

---
+++ pages_site/accueil_site.php
@@ -0,0 +1,52 @@
+<html>
+<!--Page d'acceuil du site web CRAnet-->		
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link href="../mise_en_page/bootstrap-4.3.1/dist/css/bootstrap.min.css" rel="stylesheet" media="all" type="text/css">
+		
+		<!-- Entête -->
+		<?php include("../mise_en_page/entete.html");?>	
+
+		<!--  Navigation -->
+		<?php include("../mise_en_page/navigation.html"); ?>
+	</head>
+	
+	<body>
+		
+
+		
+	<div class="container">	
+		<div class="row">
+			<div id="block1" class="col-md-3 offset-md-2">
+				<a href="https://www.synel.net/"><img src="../mise_en_page/images/synel1.png" class="rounded float-left" alt="synel"></a>
+			</div>
+			
+			<div id="block3" class="col-md-3 offset-md-2">
+				<a href="https://www.ifce.fr/"><img src="../mise_en_page/images/ifce.png" class="rounded float-right" alt="ifce"></a>
+			</div>
+		</div>
+	</div>
+	<div class="container">	
+		<div class="row">
+			<div id="block1" class="col-md-3 offset-md-2">
+				<p align='center'>
+					Accéder à SYNEL, la base de données de l’ARSOE conçue pour enregistrer et visualiser les
+					informations relatives à vos cheptels bovins.
+				</p>
+			</div>
+			<div id="block2" class="col-md-3 offset-md-2">
+				<p align='center'>
+					Accéder au SIRE, la base de données de l’IFCE vous permettant d’enregistrer et de visualiser les
+					informations concernant vos cheptels équins.
+				</p>
+			</div>
+		</div>
+	</div>
+	
+	<!-- DIV Pied de page -->
+	<?php include ("../mise_en_page/pied.html");?>
+	</body>
+	
+
+</html>

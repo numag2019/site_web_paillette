f684834eadb6619783831eec9f8a020605b1c746
pierre-alban <50011360+pierre-alban@users.noreply.github.com>
2019-05-08 13:24:32 +0200

    test avec bootstrap

---
+++ test _graphique/p1.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous"> -->
+		<!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
+		<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script> -->
+		<!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script> -->
+		
+		<link href="bootstrap-4.3.1/dist/css/bootstrap.min.css" rel="stylesheet" media="all" type="text/css"> 
+
+		<!-- <script  type="text/javascript" src="bootstrap-4.3.1/site/docs/4.3/assets/js/vendor/jquery-slim.min.js"></script> -->
+		<!-- <script  type="text/javascript" src="bootstrap-4.3.1/dist/js/bootstrap.min.js"></script> -->
+	</head>
+	
+	<body>
+		<div class="container-fluid">		
+			<div class="row">
+				<div id="block1" class="col-md-6" style="border:1px solid red">
+					<h1>Mon petit loulou</h1>
+				</div>
+				<div id="block2" class="col-md-6" style="border:1px solid red">
+					<h1>Mon petit loulou</h1>
+				</div>
+			</div>
+			<h1>Hello world</h1>
+			<h2>Hello downtown</h2>
+			<h3>Hello you</h3> 			
+			<div class="form-group">
+				<label for="ex">email</label>
+				<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+				<label for="rfnz">nom</label>
+				<input type="text" class="form-control"></input>
+				<label for="fzfbgq" class="col-md-8" align="right">options</label>
+				<input type="checkbox" class="form-control col-md-1" align="left"></input>
+			</div>
+		</div>
+
+
+	</body>
+</html>

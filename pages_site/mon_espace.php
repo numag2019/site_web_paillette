4280b5ad8204f65c958056e8de32c1dbc1765d4b
theo6197@gmail.com <49755574+theo6197@users.noreply.github.com>
2019-05-08 19:35:08 +0200

    pb error
    }

--- pages_site/mon_espace.php
+++ pages_site/mon_espace.php
@@ -51,7 +51,7 @@
 				//page pour administrer les types d'utilisateurs
 				echo "<p><a href='type_utilisateur.php'>Administer les administrateurs de Races d IA</a></p>";
 				}
-	}
+	
 	else 
 	{echo "<script type='text/javascript'>document.location.replace('authentification.php');</script>";}
 				

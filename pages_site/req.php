5ee08a12b26d7c29d07d7818b9efa2404180c542
theo6197@gmail.com <49755574+theo6197@users.noreply.github.com>
2019-05-10 18:23:17 +0200

    ghj

--- pages_site/req.php
+++ pages_site/req.php
@@ -3,9 +3,10 @@
 		$bdd = new PDO('mysql:host=localhost;dbname=crabase','root','')
 	
 		// Requête SQL sécurisée
-		$req = $bdd->prepare("UPDATE utilisateurs
+		$req = $bdd->prepare("UPDATE previsions
 							SET nbr_paillettes = :nbr_paillettes
 							WHERE id_vache= :id_vache
+							AND 
 ");
 		$req->bindValue('mdp', $mdp_hash, PDO::PARAM_STR);
 		$req->bindValue('id_utilisateur', $_SESSION['id_utilisateur'], PDO::PARAM_STR);

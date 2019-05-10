f81dcba2adc562326a3b27041d67132b9e351072
theo6197@gmail.com <49755574+theo6197@users.noreply.github.com>
2019-05-10 15:35:13 +0200

    Update affichage_pdf.php

--- pages_site/affichage_pdf.php
+++ pages_site/affichage_pdf.php
@@ -16,6 +16,7 @@
 	
 	<body>
 	<?php
+
 		try { $bdd = new PDO('mysql:host=localhost;dbname=crabase','root','', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING)); }
 	
 		catch (Exeption $e) { die('Erreur : ' . $e->getMessage())  or die(print_r($bdd->errorInfo())); }
@@ -34,13 +35,16 @@
 		$resultat = $req->fetchAll(PDO::FETCH_NAMED);
 		$resultat=$resultat[0];
 		$race=$resultat['nom_race'];
-		$chemin1= '../upload/fiche_race_'.$race.'.pdf';
-		$chemin2= 'fiche_eleveur_'.$race.'.pdf';
-		$chemin3= 'fiche_race_globale.pdf';
+		$chemin='../importexport/export/';
+		$chemin1= $chemin.'fiche_race_'.$race.'.pdf';
+		$chemin2= $chemin.'fiche_eleveur_'.$race.'.pdf';
+		$chemin3= $chemin.'fiche_race_globale.pdf';
 	
-echo '<a href='.$chemin1.'>fiche_race</a>'
-echo '<a href='.$chemin2.'>fiche_eleveur</a>'
-echo '<a href='.$chemin3.'>fiche_race_globale</a>'
+		echo '<a href='.$chemin1.'>fiche_race</a>';
+		echo "<BR>";
+		echo '<a href='.$chemin2.'>fiche_eleveur</a>';
+		echo "<BR>";
+		echo '<a href='.$chemin3.'>fiche_race_globale</a>';
  ?>
 
 

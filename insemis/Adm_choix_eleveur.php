7b1249834c0c4d531783cecfff85c265ee70b7fc
CharlotteLbt <charlotte_ls@hotmail.fr>
2019-05-13 17:26:56 +0200

    matrice ajoutée dans Adm_choix_eleveur

--- insemis/Adm_choix_eleveur.php
+++ insemis/Adm_choix_eleveur.php
@@ -81,6 +81,176 @@
 				
 					echo "<INPUT TYPE='SUBMIT' name='bt_submit_hist' value='Voir son historique de prévisions paillettes'>";
 					echo "<br><br>";
+					
+					
+					
+				//$id_eleveur = $_POST["liste_eleveurs"];
+						/*$race = $_POST["liste_race"];
+						if ($race == 6)
+							$nom_race = 'marine';
+						if ($race == 5)
+							$nom_race = 'bordelaise';
+						if ($race == 19)
+							$nom_race = 'béarnaise';*/
+						
+						$race=5;
+						$nom_race="bordelaise";
+						
+						//echo "Matrice de parenté de l'éleveur " .$nom_eleveur. " pour la race " .$nom_race;
+						echo "Matrice de parenté de l'éleveur " .$eleveur_select. " pour la race " .$nom_race;
+						//echo "<input type='hidden' name='nom_eleveur' value='".$nom_eleveur."'>";
+						//echo "<input type='hidden' name='id_eleveur' value='".$id_eleveur."'>";
+						//echo "<input type='hidden' name='id_eleveur' value='".$eleveur_select."'>";
+						//echo "<input type='hidden' name='id_race' value='".$race."'>";
+						//echo "<input type='hidden' name='nom_race' value='".$nom_race."'>";
+						//echo '<INPUT TYPE="submit" name="bouton_historique" class="btn btn-dark" value="Afficher l historique de commande de l éleveur">';
+						echo '<br>';
+						/*$query_matrice = "SELECT coefficients.id_vache, coefficients.id_taureau
+										 FROM coefficients
+										 JOIN bovins ON bovins.id_bovin = coefficients.id_vache 
+										 WHERE bovins.id_race =".$race." and bovins.id_utilisateur = ".$id_eleveur." ";*/
+						$query_matrice = "SELECT coefficients.id_vache, coefficients.id_taureau
+										 FROM coefficients
+										 JOIN bovins ON bovins.id_bovin = coefficients.id_vache 
+										 WHERE bovins.id_race =".$race." and bovins.id_utilisateur = ".$eleveur_select." ";
+						$result_matrice = mysqli_query($link, $query_matrice);
+						$tab_matrice = mysqli_fetch_all($result_matrice);
+						$nb_accouplement = mysqli_num_rows($result_matrice);
+						$liste_males = [];
+						$liste_nom_males = [];
+						for ($k=0; $k < $nb_accouplement; $k++)
+						{
+							$individu = $tab_matrice[$k][1];
+							if (in_array($individu,$liste_males))
+							{}
+							else
+							{
+								$query_nom_male = 'SELECT bovins.nom_bovin FROM bovins WHERE bovins.id_bovin='.$tab_matrice[$k][1].'';
+								$result_nom_male = mysqli_query($link, $query_nom_male);
+								$tab_nom_male = mysqli_fetch_all($result_nom_male);
+								array_push($liste_nom_males,$tab_nom_male[0][0]);
+								array_push($liste_males,$tab_matrice[$k][1]);
+								
+							}
+						}
+						
+						$liste_femelles = [];
+						$liste_nom_femelle = [];
+						for ($k=0; $k < $nb_accouplement; $k++)
+						{
+							$individu_femelle = $tab_matrice[$k][0];
+							if (in_array($individu_femelle,$liste_femelles))
+							{}
+							else 
+							{
+								$query_nom_femelle = 'SELECT bovins.nom_bovin FROM bovins WHERE bovins.id_bovin='.$tab_matrice[$k][0].'';
+								$result_nom_femelle = mysqli_query($link, $query_nom_femelle);
+								$tab_nom_femelle = mysqli_fetch_all($result_nom_femelle);
+								array_push($liste_nom_femelle,$tab_nom_femelle[0][0]);
+								array_push($liste_femelles,$tab_matrice[$k][0]);
+							}
+						}
+
+						
+						$nb_males=count($liste_males);
+						$nb_femelle=count($liste_femelles);
+						
+						echo '<div class="row">';
+						echo '<div class="col-6">';
+						echo '<table class="table table-bordered">';
+						echo '<tr>';
+						echo '<td>&nbsp;</td>';
+						for ($j=0; $j < $nb_males; $j++)
+								{
+									echo '<td>' . $liste_nom_males[$j]. '</td>';
+								}
+						echo '</tr>';
+						
+						for ($i=0; $i < $nb_femelle; $i++)
+						{
+							echo '<tr><center>';
+							echo '<td>'.$liste_nom_femelle[$i];;
+							for ($j=0; $j < $nb_males; $j++)
+								{
+								$query_color = "SELECT races.seuil_min, races.seuil_max FROM races WHERE id_race=".$race."";
+								$result_color = mysqli_query($link, $query_color);
+								$tab_color = mysqli_fetch_all($result_color);
+
+								$query_coeff="SELECT coefficients.valeur_coeff 
+														FROM coefficients 
+														WHERE id_vache=" .$liste_femelles[$i]." AND id_taureau=".$liste_males[$j]."";
+								$result_coeff = mysqli_query($link, $query_coeff);
+								$tab_coeff = mysqli_fetch_all($result_coeff);
+								$query_periode = "SELECT periodes.id_periode FROM periodes WHERE ISNULL(date_fin) =1 AND id_race =".$race."";
+								$result_periode = mysqli_query($link, $query_periode);
+								$tab_periode = mysqli_fetch_all($result_periode);
+								$periode = $tab_periode[0][0];
+								$query_prev = "SELECT previsions.nbr_paillettes FROM previsions 
+											WHERE id_vache=" .$liste_femelles[$i]." AND id_taureau=".$liste_males[$j]." AND id_periode=".$periode. "";
+								$result_prev = mysqli_query($link, $query_prev);
+								$tab_prev = mysqli_fetch_all($result_prev);
+								if(isset($tab_coeff[0][0]))
+								{
+									if ($tab_coeff[0][0]<$tab_color[0][0])
+										$color = 'green';
+									if ($tab_coeff[0][0]>$tab_color[0][0] AND $tab_coeff[0][0]<$tab_color[0][1])
+										$color = 'orange';
+									if ($tab_coeff[0][0]>$tab_color[0][1])
+										$color = 'red';
+									echo '<td bgcolor ='.$color.'><center>';
+									echo $tab_coeff[0][0];
+									if(isset($tab_prev[0][0]))
+									{
+										echo ' <br> '.$tab_prev[0][0];
+									}
+								}
+								echo '</center></td>';
+								// echo '</tr>';
+								}
+							echo '</td>';
+							echo '</center></tr>';
+						}
+						echo '</table>';
+						echo '</div>';
+						echo '</div>';
+						echo '<br>';
+						echo '<div class="row">';
+						echo '<div class="col-3">';
+						echo '<table class="table table-bordered">';
+						echo '<tr>';
+						echo '<td bgcolor=green> Accouplement très favorable </td> ';
+						echo '</tr>';
+						echo '<tr>';
+						echo '<td bgcolor=orange> Accouplement favorable </td> ';
+						echo '</tr>';
+						echo '<tr>';
+						echo '<td bgcolor=red> Accouplement peu favorable </td> ';
+						echo '</tr>';
+						echo '</table>';
+						echo '</div>';
+						echo '</div>';
+						echo '<br> <br>';	
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
 				
 				if(isset($_GET['bt_submit_hist']))
 				{

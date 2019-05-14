9244f18b8efe03dfa8c8eef2bf0515ab140d636e
Jules Poisot <jules.poisot@gmail.com>
2019-05-14 09:17:44 +0200

    Update authentification.php

--- pages_site/authentification.php
+++ pages_site/authentification.php
@@ -7,7 +7,7 @@
 		<script  type="text/javascript" src="../mise_en_page/bootstrap-4.3.1/site/docs/4.3/assets/js/vendor/jquery-slim.min.js"></script>
 		<script  type="text/javascript" src="../mise_en_page/bootstrap-4.3.1/dist/js/bootstrap.min.js"></script> 
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
-		<link rel="stylesheet" href="../mise_en_page/bootstrap.css">
+		<link rel="stylesheet" href="../mise_en_page/bootstrap2.css">
 
 	<!-- Déclaration des types d'utilisateurs autorisés à accéder à cette page -->
 	<?php $autorisation=TRUE // tout le monde?>

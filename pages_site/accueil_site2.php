c4bcb0ba569e4074515c2043fbe71ad25073bf59
Jules Poisot <jules.poisot@gmail.com>
2019-05-15 02:45:22 +0200

    Tests

--- pages_site/accueil_site2.php
+++ pages_site/accueil_site2.php
@@ -1,4 +1,4 @@
-<html>
+<html style="position: relative; min-height: 100%;">
 <!--Page d'acceuil du site web CRAnet-->		
 	<head>
 		<meta charset="utf-8">
@@ -20,9 +20,32 @@
 	 ?>
 	</head>
 
-<body class="home">
+<body class="home" style="background: url(https://cdn.pixabay.com/photo/2017/06/23/23/50/cow-2436354_1280.jpg) no-repeat center center fixed; background-size: cover; height: 100%;">
+	<div class="container">	
+		<div class="h-100 row align-items-center">
+			<div class="col-md-4 col-lg-4">
+				<a href="https://www.synel.net/"><img class="img-fluid" src="../mise_en_page/images/synel2.png"></a>
+				<a href="https://www.synel.net/" class="btn btn-light" style="background: rgba(255,255,255,0.5);">Enregistrer et visualiser les informations relatives à vos cheptels bovins.</a>
+			</div>
+			
+			<div class="col-md-4 col-lg-4">
+				<a href="http://194.199.250.44:8080/ora/main_icefaces.html?fbclid=IwAR0i_Dfnv6FnDNA9tiD1J3a6t0yc17vIprP5j0i55LLzgm0KLGKVHeK5VjU"><img class="img-fluid" src="../mise_en_page/images/mouton.jpg"></a>
+				<a href="http://194.199.250.44:8080/ora/main_icefaces.html?fbclid=IwAR0i_Dfnv6FnDNA9tiD1J3a6t0yc17vIprP5j0i55LLzgm0KLGKVHeK5VjU" class="btn btn-light" style="background: rgba(255,255,255,0.5);">Modifier les informations relatives à vos cheptels ovins.</a>
+			</div>
+			
+			<div class="col-md-4 col-lg-4">
+				<a href="https://www.ifce.fr/"><img class="img-fluid" src="../mise_en_page/images/ifce.jpg"></a>
+				<a href="https://www.ifce.fr/" class="btn btn-light" style="background: rgba(255,255,255,0.5);">Enregistrer et visualiser les informations concernant vos cheptels équins.</a>
+			</div>
+		</div>
+	</div>
+	
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
 </body>
+
+	<footer>
+		<?php include ("../mise_en_page/pied2.html");?>
+	</footer>
 
 </html>

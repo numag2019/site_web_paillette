592b52520140570fc1aeda7cd30d5e7851f249ba
Jules Poisot <jules.poisot@gmail.com>
2019-05-15 02:08:19 +0200

    Bordel

---
+++ pages_site/accueil_site2.php
@@ -0,0 +1,28 @@
+<html>
+<!--Page d'acceuil du site web CRAnet-->		
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+		<link rel="stylesheet" href="bootstrap.min.css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+
+		<!-- Custom styles for our template -->
+		<link rel="stylesheet" href="bootstrap-theme.css" media="screen" >
+		<link rel="stylesheet" href="main.css">
+		
+		<!-- Déclaration des types d'utilisateurs autorisés à accéder à cette page -->
+	<?php $autorisation=TRUE // tout le monde?>
+
+	<!--  Navigation -->
+	 <?php 
+	 include("../mise_en_page/navigation2.html"); 
+	 ?>
+	</head>
+
+<body class="home">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+</body>
+
+</html>
